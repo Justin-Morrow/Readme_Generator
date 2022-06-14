e1260e985141e14d1affbc24c18c5c5048163b00
Justin Morrow <morrowjustin19@gmail.com>
2022-06-13 19:56:54 -0700

    updating structure

--- index.js
+++ index.js
@@ -2,24 +2,51 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkDown = require('./generateMarkDown');
 const util = require('util');
+const path = require('path');
 
-// Internal modules
+let questions = [
+    {
+        type: "input",
+        message: "What is the title of your repository?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Tell about your application.",
+        name: "logo"
+    },
+    {
+        type: "input",
+        message: "Give the steps to install the application.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Write the instructions on how to use the application.",
+        name: "Use"
+    },
+    {
+        type: 'list',
+        message: 'Select the license of your application.',
+        choices: ['GNU GPLv3.0 License', 'MIT License', 'Apache-2.0 License', 'None'],
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'Please note the users who contributed to this project.',
+        name: 'contribute'
+    },
+    {
+        type: "input",
+        message: "What is your GitHub user ID?",
+        name: "logo"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+]
 
-// Inquirer prompts for userResponses
-
-
-// Main function
-async function init() {
-    try {
-
-        // Prompt Inquirer questions
-        const userResponses = await inquirer.prompt(questions);
-        console.log("Your responses: ", userResponses);
-        console.log("Thank you for your responses! Fetching your GitHub data next...");
-    
-// Call GitHub api for user info
-
-// Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
-
- // Write markdown to file
